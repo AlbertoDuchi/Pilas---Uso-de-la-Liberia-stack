7d11b45de7ea731a658aaccc46c0893dca278826
Albertordb <58319025+AlbertoDuchi@users.noreply.github.com>
2024-05-02 16:50:48 -0500

    Add files via upload

---
+++ Pila2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stack>
+#include<time.h>
+using namespace std;
+int main(){
+	stack<int> pila;
+	int tamanio=10;
+	string texto;
+	//ingreso de datos
+	srand(time(NULL));
+	for(int i=1; i<=tamanio;i++){
+		pila.push(i);
+	};
+	//numero de elementos de pila
+	cout<<"Tamanio: "<<pila.size()<<" ";
+	//ultimo elmento
+	cout<<"Ultimo elemento: "<<pila.top()<<endl;
+	pila.pop();
+	//numero de elementos de pila
+	cout<<"Tamanio: "<<pila.size()<<" ";
+	//ultimo elmento
+	cout<<"Ultimo elemento: "<<pila.top()<<endl;
+	texto=(pila.empty())?" Verdadero ":" Falso ";
+	cout<<"Pila vacia?: "<<texto<<endl;	
+	for(;!pila.empty();){
+		cout<<pila.top()<<" ";
+		pila.pop();
+	};
+	cout<<"\nTamanio: "<<pila.size()<<" ";
+	return 0;
+}
